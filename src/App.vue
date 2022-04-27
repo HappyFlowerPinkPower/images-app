b98e486e63dcdb2416c0bf8009b5a868c8927c0c
Javier <letojavier97@gmail.com>
2022-04-27 11:23:54 +0200

    deploy

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" class="p-5">
+    <h2 class="title is-2">Image test</h2>
+    <ul class="columns is-multiline">
+      <PlaceholderImage v-for="image, key in currentImages" @eventRemoveFromArray="removeFromArray" :key="key" :src="image.url" :alt="image.title" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import PlaceholderImage from './components/PlaceholderImage.vue';
+
+export default {
+  components: {
+    PlaceholderImage,
+  },
+  data() {
+    return {
+      level: 1,
+      images: []
+    }
+  },
+  computed: {
+    currentImages: function() {
+      return this.images.slice(0, this.level * 40);
+    }
+  },
+  methods: {
+    addItems() {
+      fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(data =>  data.json())
+      .then(json => this.images = this.images.concat(json))
+      .catch(error => console.error(error.message));
+    },
+    removeFromArray(id) {
+      this.images.splice(id, 1);
+    }
+  },
+  created() {
+    this.addItems();
+  },
+  mounted() {
+    window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop + window.innerHeight + 500 > document.documentElement.offsetHeight) this.level += 1;
+
+      if (this.currentImages.length + 960 > this.images.length) this.addItems();
+    });
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/PlaceholderImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <li @click="destroyImage" class="column is-half is-one-quarter-desktop">
+    <img :src="src" :alt="alt" title="Remove square!" />
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    alt: String
+  },
+  methods: {    
+    destroyImage() {
+      this.$emit('eventRemoveFromArray', this.$vnode.key);
+    }
+  }
+}
+</script>
+
+<style scoped>
+li img {
+  cursor: pointer;
+}
+</style>
